9da262a0ad1872984c014e8d5562ccb9dbb77ea9
Khalid Jamal <5aled.gamal231@gmail.com>
2020-05-28 03:09:20 +0200

    Cart Page Added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
+    //Minus and Plus Counter
+    $('.cart-sec .minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.cart-sec  .plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
     //Edit Btn
     $('#editBtn').click(function(){
         $(this).siblings('input').removeAttr('disabled');
